a24b82e94c367e37ccb2e955a640d808034d78fe
vovademarnsu <v.medvedev@g.nsu.ru>
2022-10-03 17:08:04 +0700

    lil modification

--- src/main/java/Lab9.java
+++ src/main/java/Lab9.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Lab9 {
+public class Lab9 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Philosopher[] philosophers = new Philosopher[5];
+        Object[] forks = new Object[philosophers.length];
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Object();
+        }
+        for (int i = 0; i < philosophers.length; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % forks.length];
+            if (i == philosophers.length - 1) {
+                philosophers[i] = new Philosopher(rightFork, leftFork);
+            } else {
+                philosophers[i] = new Philosopher(leftFork, rightFork);
+            }
+            Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
+            t.start();
+        }
+    }
+
+    public static class Philosopher implements Runnable {
+        private Object leftFork;
+        private Object rightFork;
+
+        public Philosopher(Object leftFork, Object rightFork) {
+            this.leftFork = leftFork;
+            this.rightFork = rightFork;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    doAction("Thinking");
+                    synchronized (leftFork) {
+                        doAction("Picked up left fork");
+                        synchronized (rightFork) {
+                            doAction("Picked up right fork - eating");
+                            doAction("Put down right fork");
+                        }
+                        doAction("Put down left fork. Back to thinking");
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        private void doAction(String action) throws InterruptedException {
+            System.out.println(Thread.currentThread().getName() + " " + action);
+            Thread.sleep(((int) (Math.random() * 1500)));
+        }
+    }
 }
